44048b72c76b4cc8945c65e569fb3d717720907f
zhiyixu <97584093+zhiyixu@users.noreply.github.com>
2025-07-14 18:44:17 +0800

    Update tokenizer.py

--- demo/tokenizer/tokenizer.py
+++ demo/tokenizer/tokenizer.py
@@ -1,142 +1,141 @@
-from collections import OrderedDict 
-from collections import Counter
+from collections import OrderedDict
 from datetime import datetime
-import pdb
+import numpy as np
+from typing import Optional, Union
+import os, sys, time, json
+
+class Token():
+
+    def __init__(self, vocab: Union[list[int], list[list[int]]], idx: int):
+        self.vocab = vocab
+        self.idx = idx
 
 
-data_file_name="./data.txt"
+class Tokenizer():
 
-def kmp_search(main_list, sublist):
-    def compute_lps(pattern):
-        lps = [0] * len(pattern)
-        length = 0
-        i = 1
-        while i < len(pattern):
-            if pattern[i] == pattern[length]:
-                length += 1
-                lps[i] = length
-                i += 1
-            else:
-                if length != 0:
-                    length = lps[length - 1]
-                else:
-                    lps[i] = 0
-                    i += 1
-        return lps
+    def __init__(self, vocab_size: int=512):
+        self.vocab_dict = OrderedDict()
+        self.vocab_size = vocab_size
+        self.txt: list = []
+        self.tokenizer_file = "./tokenizer.json"
 
-    lps = compute_lps(sublist)
-    count = 0
-    i = j = 0
+        # methods
+        self._init_vocab_dict()
 
-    while i < len(main_list):
-        if sublist[j] == main_list[i]:
-            i += 1
-            j += 1
-
-        if j == len(sublist):
-            count += 1
-            j = lps[j - 1]
-        elif i < len(main_list) and sublist[j] != main_list[i]:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
-
-    return count
-
-
-
-
-def update_vocab(d: dict=OrderedDict(), new_vocab: list=[]) -> tuple:
-    if len(d) == 0: # 初始化
+    def _init_vocab_dict(self):
+        """初始化词表，添加基础词"""
         for i in range(256):
-            d[i] = [i] 
-        return d, None 
-    else:  # 存在则找到最大值后新增
-        if new_vocab not in list(d.values()):
-            max_id = len(d)
-            d[max_id] = new_vocab
-            return d, max_id 
-        else:
-            return d, len(d)
-    
+            self.vocab_dict[i] = [i]
 
-def read_data(file_name):
-    """
-    以二进制的方式读取数据
-    """
-    with open(file_name, 'rb') as file:
-        data = file.read()
-    return data
 
-def take_gram(txt: list[int])-> list[list]: # 返回嵌套列表
-    """
-    将字节序列转换为二元组
-    """
-    return [[txt[i], txt[i+1]] for i in range(0,len(txt)-1)]
 
-def find_max_freq(txt: list[int], gram_list: list[list]) -> list:
-    """
-    找当当前情况下的最高频率的词
-    """
-    max_freq = 0
-    vocab: list[int] = []
-    for gram in gram_list:
-        c = 0
-        for i in range(0, len(txt)-1):
-            if [txt[i], txt[i+1]] == gram:
-                c+=1
-        # c = kmp_search(txt, gram)
-        if c>=max_freq: # 如果出现更高的频率
-            max_freq = c # 更新当前最高频率  
-            vocab = gram # 记录这个词
-    return vocab 
-    
-def get_next_id(d: dict):
-    """为新找到的vocab分配一个id"""
-    return max(list(d.keys())) + 1
+    def _read_data(self, fname: str):
+        if not os.path.exists(fname):
+            print(f"{datetime.now()} File: {fname} Not Exists!")
+            sys.exit(-1)
+        if not fname.endswith(".txt"):
+            print(f"{datetime.now()} Current Support .txt Only.")
+            sys.exit(-1)
+        with open(fname, "r", encoding="utf-8") as f:
+            contents = f.read()
+        return contents.encode("utf-8")
 
-def update_txt(txt_b: list, new_vocab: list[list], max_id: int):
-    """用新找到的vocab来替换原始的数据"""
-    new_txt_b = []
-    sindex = 0
-    while 1:
-        if sindex < len(txt_b)-1:  # 还没到最后交界点
-            if [txt_b[sindex], txt_b[sindex+1]] == new_vocab:  # 需要合并的词
-                new_txt_b.append(max_id)
-                sindex += 2
-            else:
-                new_txt_b.append(txt_b[sindex])
+    def set_contents(self, databytes: bytes=b''):
+        self.txt = list(databytes)
+
+    def take_gram(self) -> list[list]:
+        """将字节序列转换为二元列表"""
+        return [[self.txt[i], self.txt[i + 1]] for i in range(0, len(self.txt) - 1)]
+
+    def find_max_freq(self, gram_list: list[list[int]]) -> list:
+        """找到当前内容中频率最高的词"""
+        max_freq = 0
+        vocab: list[int] = []
+        np_txt = np.array(self.txt, dtype=int)  # 这里需要动态生成， 因为txt会被合并
+        for gram in gram_list:
+            first_matches = np_txt[:-1] == gram[0]
+            last_matches = np_txt[1:] == gram[1]
+            cnt = np.count_nonzero(first_matches & last_matches)
+            if cnt >= max_freq:  # 如果出现更高的频率
+                max_freq = cnt  # 更新当前最高频率
+                vocab = gram  # 记录这个词
+        return vocab
+
+    def update_txt(self, new_vocab: list[list], max_id: int):
+        new_txt_b = []
+        sindex = 0
+        while 1:
+            if sindex < len(self.txt) - 1:  # 还没到最后交界点
+                if [self.txt[sindex], self.txt[sindex + 1]] == new_vocab:  # 需要合并的词
+                    new_txt_b.append(max_id)
+                    sindex += 2
+                else:
+                    new_txt_b.append(self.txt[sindex])
+                    sindex += 1
+            elif sindex == len(self.txt) - 1:  # 刚好是最后一个
+                new_txt_b.append(self.txt[sindex])
                 sindex += 1
-        elif sindex == len(txt_b)-1:  # 刚好是最后一个
-            new_txt_b.append(txt_b[sindex])
-            sindex += 1
-        else: # 没了
-            break
-    return new_txt_b
+            else:  # 没了
+                break
+        self.txt = new_txt_b # update
+
+    def update_vocab(self, new_vocab: []) -> int:
+        max_id = len(self.vocab_dict)
+        if new_vocab not in list(self.vocab_dict.values()):
+            self.vocab_dict[max_id] = new_vocab
+        return max_id
+
+    def train(self):
+        print(f"{datetime.now()} Start Train Tokenizer....")
+        t0 = time.time()
+        while len(self.vocab_dict) < self.vocab_size:
+            gran_2 = self.take_gram()
+            new_vocab = self.find_max_freq(gram_list=gran_2)
+            max_id = self.update_vocab(new_vocab=new_vocab)
+            self.update_txt(new_vocab=new_vocab, max_id=max_id)
+            print(f"{datetime.now()} current vocab size: {len(self.vocab_dict)}, new id: {max_id}")
+        t1 = time.time()
+        print(f"{datetime.now()} Tokenizer Training Finished, Cost: {t1-t0:.2f}")
+        with open(self.tokenizer_file,"w") as f:
+            json.dump(self.vocab_dict, f, indent=4, ensure_ascii=False)
+        print(f"{datetime.now()} Tokenizer File Save To: {self.tokenizer_file}")
+
+    def encode(self, raw_txt: str):
+        contents = list(raw_txt.encode())
+        for idx, vocab in list(self.vocab_dict.items())[::-1]:
+            ll = []
+            start_index = 0
+            while 1:
+                if start_index < len(contents) - 1:
+                    if [contents[start_index], contents[start_index + 1]] == vocab:
+                        ll.append(idx)
+                        start_index += 2
+                    else:
+                        ll.append(contents[start_index])
+                        start_index += 1
+                elif start_index == len(contents) - 1:
+                    ll.append(contents[start_index])
+                    start_index += 1
+                else:
+                    break
+            contents = ll  # 更新内容
+        return contents
+
+    def decode(self, idx_list: list):
+        ll = []
+        for idx in idx_list:
+            ll += self.vocab_dict[idx]
+        return bytes(ll).decode()
 
 
-
-txt = read_data(data_file_name)
-
-txt_b = list(txt)
-vocab = list(set(txt_b))
-vocab.sort() 
-vocab_dict, _ = update_vocab()
-for i in vocab:
-    vocab_dict, _ = update_vocab(d=vocab_dict, new_vocab=[i])
-
-while len(vocab_dict) < 512:
-    # pdb.set_trace()
-    gran_2 = take_gram(txt=txt_b)
-    new_vocab = find_max_freq(txt=txt_b, gram_list=gran_2)
-    vocab_dict, max_id = update_vocab(d=vocab_dict, new_vocab=new_vocab)
-    txt_b = update_txt(txt_b=txt_b, new_vocab=new_vocab, max_id=max_id)
-    print(f"{datetime.now()} current vocab size: {len(vocab_dict)}, new id: {max_id}")
-
-pdb.set_trace()
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
+if __name__ == "__main__":
+    data = "./data.txt"
+    tokenizer = Tokenizer()
+    data_b = tokenizer._read_data(fname=data)
+    tokenizer.set_contents(databytes=data_b)
+    tokenizer.train()
+    text_txt = "北京奥运会志愿者标志发布，志愿者项目正式启动。6月26日，北京奥组委宣布第29届奥运会主题口号：“同一个世界，同一个梦想”（One World，One Dream）。11月11日，第29届奥运会吉祥物在北京公布"
+    idx_list = tokenizer.encode(raw_txt=text_txt)
+    print(f"len raw_txt: {len(text_txt)}, len raw bytes: {len(tokenizer.txt)}, len encode idx: {len(idx_list)}")
+    decoded_txt = tokenizer.decode(idx_list=idx_list)
+    print(f"Decoded contents: {decoded_txt}")
